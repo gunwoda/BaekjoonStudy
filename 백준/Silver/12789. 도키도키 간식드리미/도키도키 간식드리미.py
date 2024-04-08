6886abf008da66aedc53246ced9a4b59161adbff
gunwoda <95297035+gunwoda@users.noreply.github.com>
2024-04-08 15:51:13 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -1,15 +1,16 @@
-n = int(input())
-lst = list(map(int,input().split()))
-count = 1
-stack = []
 
-for i in lst:
-    stack.append(i)
-    while stack and stack[-1] == count:
+n = int(input())
+
+lst = list(map(int,input().split()))
+stack = []
+count = 1
+for i in range(n):
+    stack.append(lst[i])
+    while stack and stack[-1]==count:
         stack.pop()
         count = count+1
 
-if stack:
-    print('Sad')
-else:
+if len(stack) == 0:
     print('Nice')
+else :
+    print('Sad')
