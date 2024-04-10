75cb6c36bb88f525d10509aecacdd20c2c2a5c76
gunwoda <95297035+gunwoda@users.noreply.github.com>
2024-04-10 23:35:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 504 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,41 @@
 from collections import deque
 
-a,b,c = map(int,input().split())
-lst = [[] for _ in range(a)]
-for _ in range(b):
-    p,q = map(int,input().split())
-    lst[p-1].append(q-1)
-    lst[q-1].append(p-1)
+n,m,v = map(int,input().split())
+lst = [[] for _ in range(n+1)]
+for i in range(m):
+    a,b = map(int,input().split())
+    lst[a].append(b)
+    lst[b].append(a)
+
 for i in lst:
     i.sort()
-def dfs_process(start_node):
-    visited = [0 for _ in range(a)]
-    ret_lst = []
 
-    def dfs(start_node):
-        visited[start_node] = 1
-        ret_lst.append(start_node)
-        for i in lst[start_node]:
-            if i not in ret_lst:
-                dfs(i)
-    dfs(start_node)
-    result_lst = []
-    for i in ret_lst:
-        result_lst.append(i+1)
-    print(*result_lst)
 
-def bfs_process(start_node):
-    visited = [0 for _ in range(a)]
-    queue = deque([start_node])
-    ret_lst = []
-    while queue:
-        k = queue.popleft()
-        if k+1 not in ret_lst: 
-            ret_lst.append(k+1)
-        visited[k] = 1
-        for i in lst[k]:
-            if visited[i] == 0:
-                queue.append(i)
-    print(*ret_lst)
-dfs_process(c-1)
-bfs_process(c-1)
+visited = [0]*(n+1)
+answer = []
+def dfs(start_node):
+    answer.append(start_node)
+    visited[start_node] = 1
+    for i in lst[start_node]:
+        if not visited[i]:
+            dfs(i)
+
+dfs(v)
+print(*answer)
+
+visited = [0]*(n+1)
+answer = []
+
+def bfs(start_node):
+    deq = deque()
+    deq.append(start_node)
+    while deq:
+        node = deq.popleft()
+        visited[node] = 1
+        answer.append(node)        
+        for i in lst[node]:
+            if not visited[i] and i not in deq:
+                deq.append(i)
+
+bfs(v)
+print(*answer)
